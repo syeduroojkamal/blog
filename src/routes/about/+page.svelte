072591656f1fc727afab6870606e5a74a4858ab6
Syed Urooj Kamal <syeduroojkamal26@gmail.com>
2026-05-14 12:40:04 +0530

    update: about page into .md

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,19 +1,9 @@
-<div class="flex justify-center">
-    <div class="max-w-xl m-5 p-10 border rounded">
-        <h1>I’m Syed Urooj Kamal,</h1>
-        <ul class="list-disc">
-            <li>
-                Someone who enjoys technology, learning new things, and sharing
-                ideas.
-            </li>
-            <li>
-                I created this blog as a place to write about my thoughts,
-                experiences, projects, and anything I find interesting.
-            </li>
-            <li>
-                Whether it’s tech, life, or random discoveries, this space
-                reflects a little part of my journey.
-            </li>
-        </ul>
+<script>
+    import MD from "./about.md";
+</script>
+
+<div class="max-w-5xl mx-auto p-8">
+    <div class="markdown-content">
+        <MD />
     </div>
 </div>
---
+++ src/routes/about/about.md
@@ -0,0 +1,4 @@
+# About me
+- Someone who enjoys technology, learning new things, and sharing ideas.
+- I created this blog as a place to write about my thoughts, experiences, projects, and anything I find interesting.
+- Whether it's tech, life, or random discoveries, this space reflects a little part of my journey.
